bd12aa869e8a3f646d98e074dc0ecf8772dd3837
Abdessalam <Abdessalam@192.168.148.117>
2023-04-07 20:07:22 +0000

    Second commit

---
+++ src/hwtest2/DemoProject2.java
@@ -0,0 +1,13 @@
+package hwtest2;
+
+public class DemoProject2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is demo project2");
+		
+		
+		System.out.println("hi there ");
+		
+	}
+}
